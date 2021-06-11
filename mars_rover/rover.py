7e3a8edd19eb234b9e8392df441bad1efe32929f
EduardoH <Eduxtreme16@gmail.com>
2021-06-11 18:52:42 +0200

    ENH: Get direction method defined

--- mars_rover/rover.py
+++ mars_rover/rover.py
@@ -2,5 +2,8 @@ class Rover:
     def __init__(self, initial_y, initial_x, initial_direction):
         pass
 
+    def get_direction(self):
+        pass
+
     def rotate(self, rotation_direction):
         pass
