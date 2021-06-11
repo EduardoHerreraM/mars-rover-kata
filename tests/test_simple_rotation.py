2507c813c598b5fc7fc3bd3b675f4a789b87c656
EduardoH <Eduxtreme16@gmail.com>
2021-06-11 19:02:26 +0200

    ENH: Added all tests for rotation

--- tests/test_simple_rotation.py
+++ tests/test_simple_rotation.py
@@ -1,6 +1,18 @@
 from mars_rover.rover import Rover
 
 
+def test_north_to_west():
+    initial_y, initial_x = 0, 0
+    initial_direction = 'N'
+
+    rover = Rover(initial_y=initial_y, initial_x=initial_x, initial_direction=initial_direction)
+
+    rover.rotate('L')
+    final_direction = rover.get_direction()
+
+    assert final_direction == 'W'
+
+
 def test_north_to_east():
 
     initial_y, initial_x = 0, 0
@@ -14,13 +26,73 @@ def test_north_to_east():
     assert final_direction == 'E'
 
 
-def test_north_to_west():
+def test_east_to_north():
     initial_y, initial_x = 0, 0
-    initial_direction = 'N'
+    initial_direction = 'E'
 
     rover = Rover(initial_y=initial_y, initial_x=initial_x, initial_direction=initial_direction)
 
     rover.rotate('L')
     final_direction = rover.get_direction()
 
+    assert final_direction == 'N'
+
+
+def test_east_to_south():
+    initial_y, initial_x = 0, 0
+    initial_direction = 'E'
+
+    rover = Rover(initial_y=initial_y, initial_x=initial_x, initial_direction=initial_direction)
+
+    rover.rotate('R')
+    final_direction = rover.get_direction()
+
+    assert final_direction == 'S'
+
+
+def test_south_to_east():
+    initial_y, initial_x = 0, 0
+    initial_direction = 'S'
+
+    rover = Rover(initial_y=initial_y, initial_x=initial_x, initial_direction=initial_direction)
+
+    rover.rotate('L')
+    final_direction = rover.get_direction()
+
+    assert final_direction == 'E'
+
+
+def test_south_to_west():
+    initial_y, initial_x = 0, 0
+    initial_direction = 'S'
+
+    rover = Rover(initial_y=initial_y, initial_x=initial_x, initial_direction=initial_direction)
+
+    rover.rotate('R')
+    final_direction = rover.get_direction()
+
     assert final_direction == 'W'
+
+
+def test_west_to_south():
+    initial_y, initial_x = 0, 0
+    initial_direction = 'W'
+
+    rover = Rover(initial_y=initial_y, initial_x=initial_x, initial_direction=initial_direction)
+
+    rover.rotate('L')
+    final_direction = rover.get_direction()
+
+    assert final_direction == 'S'
+
+
+def test_west_to_north():
+    initial_y, initial_x = 0, 0
+    initial_direction = 'W'
+
+    rover = Rover(initial_y=initial_y, initial_x=initial_x, initial_direction=initial_direction)
+
+    rover.rotate('R')
+    final_direction = rover.get_direction()
+
+    assert final_direction == 'N'
