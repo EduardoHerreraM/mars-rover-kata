e76852fa69349e7f6f873f077c4e8f4754ae36d0
EduardoH <Eduxtreme16@gmail.com>
2021-06-11 18:54:01 +0200

    ENH: Added logic required to pass the test

--- mars_rover/rover.py
+++ mars_rover/rover.py
@@ -1,9 +1,11 @@
 class Rover:
     def __init__(self, initial_y, initial_x, initial_direction):
-        pass
+        self.y = initial_y
+        self.x = initial_x
+        self.direction = initial_direction
 
     def get_direction(self):
-        pass
+        return self.direction
 
     def rotate(self, rotation_direction):
-        pass
+        self.direction = 'E'
