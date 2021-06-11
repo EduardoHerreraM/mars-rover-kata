6715bb0921d83a67864ff074f7bcf6464cce66f3
EduardoH <Eduxtreme16@gmail.com>
2021-06-11 18:54:54 +0200

    ENH: Second test

--- tests/test_simple_rotation.py
+++ tests/test_simple_rotation.py
@@ -12,3 +12,15 @@ def test_north_to_east():
     final_direction = rover.get_direction()
 
     assert final_direction == 'E'
+
+
+def test_north_to_west():
+    initial_y, initial_x = 0, 0
+    initial_direction = 'N'
+
+    rover = Rover(initial_y=initial_y, initial_x=initial_x, initial_direction=initial_direction)
+
+    rover.rotate('L')
+    final_direction = rover.get_direction()
+
+    assert final_direction == 'W'
