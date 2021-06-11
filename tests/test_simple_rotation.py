eee44cc7ca0f6a482c01b98b4b16cce5ecfbfa20
EduardoH <Eduxtreme16@gmail.com>
2021-06-11 18:45:44 +0200

    ENH: First simple test
    First contact with pytest

---
+++ tests/test_simple_rotation.py
@@ -0,0 +1,14 @@
+from mars_rover.rover import Rover
+
+
+def test_north_to_east():
+
+    initial_y, initial_x = 0, 0
+    initial_direction = 'N'
+
+    rover = Rover(initial_y=initial_y, initial_x=initial_x, initial_direction=initial_direction)
+
+    rover.rotate('R')
+    final_direction = rover.get_direction()
+
+    assert final_direction == 'E'
