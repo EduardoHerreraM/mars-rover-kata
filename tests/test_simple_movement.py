92d0763cdd4c7c9cdab362bb7698227237f8c7f2
EduardoH <Eduxtreme16@gmail.com>
2021-06-11 19:12:28 +0200

    ENH: Added second movement test

--- tests/test_simple_movement.py
+++ tests/test_simple_movement.py
@@ -11,3 +11,15 @@ def test_move_up():
     final_position_y, final_position_x = rover.get_position()
 
     assert (final_position_y, final_position_x) == (1, 0)
+
+
+def test_move_down():
+    initial_y, initial_x = 0, 0
+    initial_direction = 'N'
+
+    rover = Rover(initial_y=initial_y, initial_x=initial_x, initial_direction=initial_direction)
+
+    rover.move(direction='b')
+    final_position_y, final_position_x = rover.get_position()
+
+    assert (final_position_y, final_position_x) == (-1, 0)
