d0aa5ec179f59e1d614a76daaea6e0ba54d4bc54
EduardoH <Eduxtreme16@gmail.com>
2021-06-11 19:22:27 +0200

    ENH: Added all simple movement tests

--- tests/test_simple_movement.py
+++ tests/test_simple_movement.py
@@ -1,7 +1,7 @@
 from mars_rover.rover import Rover
 
 
-def test_move_up():
+def test_north_move_forward():
     initial_y, initial_x = 0, 0
     initial_direction = 'N'
 
@@ -13,7 +13,7 @@ def test_move_up():
     assert (final_position_y, final_position_x) == (1, 0)
 
 
-def test_move_down():
+def test_north_move_backward():
     initial_y, initial_x = 0, 0
     initial_direction = 'N'
 
@@ -23,3 +23,75 @@ def test_move_down():
     final_position_y, final_position_x = rover.get_position()
 
     assert (final_position_y, final_position_x) == (-1, 0)
+
+
+def test_east_move_forward():
+    initial_y, initial_x = 0, 0
+    initial_direction = 'E'
+
+    rover = Rover(initial_y=initial_y, initial_x=initial_x, initial_direction=initial_direction)
+
+    rover.move(movement_direction='f')
+    final_position_y, final_position_x = rover.get_position()
+
+    assert (final_position_y, final_position_x) == (0, 1)
+
+
+def test_east_move_backward():
+    initial_y, initial_x = 0, 0
+    initial_direction = 'E'
+
+    rover = Rover(initial_y=initial_y, initial_x=initial_x, initial_direction=initial_direction)
+
+    rover.move(movement_direction='b')
+    final_position_y, final_position_x = rover.get_position()
+
+    assert (final_position_y, final_position_x) == (0, -1)
+
+
+def test_south_move_forward():
+    initial_y, initial_x = 0, 0
+    initial_direction = 'S'
+
+    rover = Rover(initial_y=initial_y, initial_x=initial_x, initial_direction=initial_direction)
+
+    rover.move(movement_direction='f')
+    final_position_y, final_position_x = rover.get_position()
+
+    assert (final_position_y, final_position_x) == (-1, 0)
+
+
+def test_south_move_backward():
+    initial_y, initial_x = 0, 0
+    initial_direction = 'S'
+
+    rover = Rover(initial_y=initial_y, initial_x=initial_x, initial_direction=initial_direction)
+
+    rover.move(movement_direction='b')
+    final_position_y, final_position_x = rover.get_position()
+
+    assert (final_position_y, final_position_x) == (1, 0)
+
+
+def test_west_move_forward():
+    initial_y, initial_x = 0, 0
+    initial_direction = 'W'
+
+    rover = Rover(initial_y=initial_y, initial_x=initial_x, initial_direction=initial_direction)
+
+    rover.move(movement_direction='f')
+    final_position_y, final_position_x = rover.get_position()
+
+    assert (final_position_y, final_position_x) == (0, -1)
+
+
+def test_west_move_backward():
+    initial_y, initial_x = 0, 0
+    initial_direction = 'W'
+
+    rover = Rover(initial_y=initial_y, initial_x=initial_x, initial_direction=initial_direction)
+
+    rover.move(movement_direction='b')
+    final_position_y, final_position_x = rover.get_position()
+
+    assert (final_position_y, final_position_x) == (0, 1)
